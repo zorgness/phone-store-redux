0f8a773612e83c2d6a6ff60679ff5d4c9f6232e7
Thomas Fouquert <tfouquert@gmail.com>
2022-09-13 09:37:54 +0200

    configStore

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 import PhoneComponent from './components/PhoneComponent';
 import TabletComponent from './components/TabletComponent';
 import TvComponent from './components/TvComponent';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
+
   return (
     <div className="section-one">
+    <Provider store={store}>
 
-      <PhoneComponent />
-      <TabletComponent />
-      <TvComponent />
 
+        <PhoneComponent />
+        <TabletComponent />
+        <TvComponent />
+
+
+    </Provider>
     </div>
   );
 }
--- src/components/PhoneComponent.js
+++ src/components/PhoneComponent.js
@@ -1,19 +1,42 @@
 import React from 'react'
+// import { connect } from 'react-redux'
 import Phone from '../images/phone.png'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { buyPhone } from '../redux/phone/actionPhone'
 
 const PhoneComponent = () => {
+
+  const count = useSelector(state => state.phones)
+  const dispatch = useDispatch()
+
+
   return (
     <div className="container">
 
       <img src={Phone} alt="phone" />
 
-      <p>Available: <span id="count"></span> </p>
+      <p>Available: <span id="count">{count}</span> </p>
 
-      <button>Buy phone</button>
+      <button onClick={() => dispatch(buyPhone())}>Buy phone</button>
 
     </div>
   )
 }
+
+// const mapStateToProps = (state) => {
+
+//   return {
+//     phones: state.phones
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+
+//   return {
+//     buyPhone: () => dispatch(buyPhone())
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps )(PhoneComponent)
 
 export default PhoneComponent
